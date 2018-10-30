304c0135c25ad757042d3ae4b0295a98503caed7
idc9 <idc9@cornell.edu>
2018-10-30 10:20:03 -0400

    better doc

--- doc/figures/make_readme_figures.py
+++ doc/figures/make_readme_figures.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 
 from diproperm.DiProPerm import DiProPerm
 
-X, y = make_blobs(n_samples=100, n_features=2, centers=2, cluster_std=2)
+X, y = make_blobs(n_samples=100, n_features=2, centers=2, cluster_std=2,
+                  random_state=20)
 plt.scatter(X[:, 0], X[:, 1], c=y)
 plt.savefig('data.png')
 plt.close()
