dc0bc7c1c8062f58547d24ed5a132362d9f3e2b1
idc9 <idc9@cornell.edu>
2018-11-05 17:53:53 -0500

    prep for 0.3.0

--- changelog.md
+++ changelog.md
@@ -1,3 +1,10 @@
+# 11/5/18 (version 0.0.3)
+
+Now saves permutation scores. A number of changes to the API and backend.
+
+- save permutation scores
+- new visualization diagnostics
+
 # 10/30/18 (version 0.0.2)
 - changes attribute names (now test_stats_, perm_samples_)
 - added custom test statistics
--- setup.py
+++ setup.py
@@ -1,21 +1,130 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# def readme():
-#     with open('README.rst') as f:
-#            return f.read()
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
 
-install_requires = ['numpy', 'sklearn', 'statsmodels']
+import io
+import os
+import sys
+from shutil import rmtree
 
-setup(name='diproperm',
-      version='0.0.2',
-      description='Implements DiProPerm for high dimensional hypothesis testing.',
-      author='Iain Carmichael',
-      author_email='idc9@cornell.edu',
-      license='MIT',
-      packages=find_packages(),
-      install_requires=install_requires,
-      test_suite='nose.collector',
-      tests_require=['nose'],
-      url='https://github.com/idc9/diproperm',
-      download_url='https://github.com/idc9/diproperm/tarball/0.0.2',
-      zip_safe=False)
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'diproperm'
+DESCRIPTION = 'DiProPerm for high dimensional hypothesis testing.'
+URL = 'https://github.com/idc9/diproperm'
+EMAIL = 'idc9@cornell.edu'
+AUTHOR = 'Iain Carmichael'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = '0.0.3'
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    'numpy', 'scipy', 'seaborn', 'matplotlib', 'sklearn'
+]
+
+# What packages are optional?
+EXTRAS = {
+
+}
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
+
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Intended Audience :: Science/Research'
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
+)
